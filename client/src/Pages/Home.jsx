1ecce18c25a96d28134c85f5218f95a878ed4e2c
Rayane <sirri.rayane.ma@gmail.com>
2026-05-30 10:15:28 +0100

    Sidebar message reworked

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,16 +1,30 @@
 import { useEffect } from "react";
 import "../css/Home.css";
-import SideBar from "../Components/SideBar";
+
+import { Search } from "lucide-react";
 
 export default function Home() {
-
   useEffect(() => {
     document.title = "Home | Chatty - Chat App";
-    document.documentElement.setAttribute('data-theme', "Light")
-  }, [])
+    document.documentElement.setAttribute("data-theme", "Light");
+  }, []);
 
-
-  return <div className="HomeContainer">
-    <SideBar />
-  </div>;
+  return (
+    <div className="HomeContainer">
+      <div className="SideBar">
+        <div className="SearchBar">
+          <div className="Title">
+            <p>Messages</p>
+          </div>
+          <div className="Bar">
+            <Search />
+            <input type="text" placeholder="Search conversations..." />
+          </div>
+        </div>
+        <div className="FriendsList">
+          <div className="FriendTemplate"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
